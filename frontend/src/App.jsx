48eb76434b59eb8070080cfe08c0b6270a984e64
Chad Manuel <chadc_manuel@yahoo.com>
2023-09-18 13:18:40 -0700

    Installed router react dom and added routes inside of appjsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/Home.jsx'
+import CreateBooks from './components/CreateBooks.jsx'
+import ShowBook from './components/ShowBook.jsx'
+import EditBook from './components/EditBook.jsx'
+import DeleteBook from './components/DeleteBook.jsx'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>Hello</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
