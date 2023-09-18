33a8797258366f592fabedb6f189db91984af03c
Chad Manuel <chadc_manuel@yahoo.com>
2023-09-18 13:11:34 -0700

    Started react project via vite

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,32 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
-import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors'
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+
+// Option 2: Allow custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) =>{
     console.log(request)
     return response.status(234).send('Welcome to MERN Stack Tutorial')
 });
-
 
 app.use('/books', booksRoute);
 
